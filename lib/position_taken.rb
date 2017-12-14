5c06eb5725632159fdb16b9f117b77919451b6d1
Amy Carney <amy.carney@alaska.gov>
2017-12-14 18:31:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8
-    false
-  elsif board[index] == " " || "" || nil
-    true
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
 end
